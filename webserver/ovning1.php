33b3ea1f1b3ea1246ea6d2f06131eabdac6a7e7d
carl-beep-cloud <carl.hoel@elev.ga.ntig.se>
2024-08-29 13:06:08 +0200

    började övning 1

---
+++ webserver/ovning1.php
@@ -0,0 +1,48 @@
+<h1> upgift 1</h1>
+
+<?php
+
+$fahreinhei = 30;
+
+
+
+ echo $celcius =5/9 *($fahreinhei - 32);
+
+
+
+
+?>
+
+<hr>
+<h1> upgift 2 </h1>
+
+
+<?php
+/*
+Starta med följande variabler:
+	$fnamn = “Kalle”;
+	$enamn = “Anka”;
+	$tal = 16;
+	$road = “Ankvägen”;
+
+Gör så att följande text skrivs ut på webbsidan:
+Kalle Anka bor på Ankvägen 16
+genom att använda dig utav variablerna.
+Testa att använda 3 olika sätt för att skriva ut.
+Dubbelfnuttar (citat-tecken “), “abc $variabel1 def $variabel2 ghi”
+Enkelfnuttar (apostrof ‘), ‘abc $variabel1 def $variabel2 ghi’
+Konkatenering (punkt .) “abc” . $variabel1 .  “def” . $variabel2 . “ghi” 
+Vilket sätt fungerar inte och varför?
+
+*/
+
+
+$fnamn = “Kalle”;
+$enamn = “Anka”;
+$tal = 16;
+$road = “Ankvägen”;
+
+//echo ("$fnamn " + "$enamn " + "$tal " + "$road");
+
+ echo ("$fnamn") + ("$enamn") + ("$tal") + ($road);
+?>
