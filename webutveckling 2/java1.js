e8715f374a1b9c9d202c7dcd767bbdb6ccf5984c
carl-beep-cloud <carl.hoel@elev.ga.ntig.se>
2024-08-30 09:33:32 +0200

    allting gjorde lite arbete japp

---
+++ webutveckling 2/java1.js
@@ -0,0 +1,23 @@
+function slav()
+{
+
+    let ett = document.getElementById("ett").value;
+    document.getElementById("tva").value= ett;
+
+          document.getElementById("ett").value = "";
+}
+
+
+function tjo()
+{
+let a = document.getElementById("a").value;
+let b = document.getElementById("b").value;
+
+a=parseInt(a);
+b=parseInt(b);
+
+let c = document.getElementById("c").value = a+b;
+
+
+
+}
