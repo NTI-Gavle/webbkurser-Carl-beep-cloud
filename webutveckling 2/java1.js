ba9da5a0eba26f4727017d22b97f27f6cb347914
carl-beep-cloud <carl.hoel@elev.ga.ntig.se>
2024-09-06 09:46:51 +0200

    jag jobbade vidare och det gick ok

--- webutveckling 2/java1.js
+++ webutveckling 2/java1.js
@@ -7,7 +7,7 @@ function slav()
           document.getElementById("ett").value = "";
 }
 
-
+// up 2
 function tjo()
 {
 let a = document.getElementById("a").value;
@@ -19,10 +19,12 @@ b=parseInt(b);
 let c = document.getElementById("c").value = a+b;
 
 
+document.getElementById("a").value = "";
+document.getElementById("b").value = "";
 
 }
 
-
+// up 3
 function skr()
 {
 
@@ -58,7 +60,7 @@ document.getElementById("fan").innerHTML = g;
 
 
 }
-
+// up 4
 
 setInterval(tiden,1000);
 
@@ -72,7 +74,7 @@ i++;
 
 document.getElementById("tide").innerHTML    = "Du har varit inne på den här sidan i  " + i + " sekunder";
 
-console.log(i);
+//console.log(i);
 }
 
 
@@ -91,29 +93,136 @@ document.getElementsByTagName("body")[0].appendChild(crsq);
 
 }
 
-
-
+// upgift 6
+let of = false;
+let k = 0;
+ let ojsan;
+ let timeout;
 function start()
 {
+   if (of == false) {
+    
+    ojsan = setInterval(tir,1000);
 
-setInterval(tir,1000);
+   }
+   of = true;
 
+    
 }
 
-let k = 0;
+
+
 function tir()
 {
 
-    k++
-    document.getElementById("ti").innerHTML = k;
+k++;
+document.getElementById("ti").innerHTML = k;
+
+}
+
+function paus()
+{
+
+
+ timeout = setTimeout(tir,500000);
+
+}
+
+function play()
+{
+
+clearTimeout(timeout);
+
 
 }
 
 function stop()
 {
+ clearInterval(ojsan);
+ k =0;
+ of = false;
+ document.getElementById("ti").innerHTML = 0;
 
+}
+
+
+
+
+/* upgift 7 
+
+Utveckla en timer  sidan där man väljer minut och sekund (det är okej att använda 2 textrutor). Klicka på starta för att börja räkna ner till 0. Skriv gärna ut nedräkningen någonstans samt skriv ut något kul när timern når 0.
+*/
+/*
+function starta()
+{
+
+    let H = document.getElementById("Hour").value;
+    let S = document.getElementById("Seee").value;
+
+    HH =parseInt(H);
+SS = parseInt(S);
+
+
+  let timmar =  document.getElementById("ho").innerHTML= HH;
+ let sekunderna =    document.getElementById("se").innerHTML = SS;
+
+    document.getElementById("Hour").value = "";
+    document.getElementById("Seee").value = "";
+
+    for (let index = 0; index < array.length; index++) {
+      
+        
+    }
+}
+*/
+
+let intervals = setInterval(countdown,1000);
+let timers = [];
+
+function startTimer()
+{
+let m = parseInt( document.getElementById("minutes").value);
+let s = parseInt( document.getElementById("secondes").value);
+
+let time = m*60+s;
+ 
+timers.push(time);
 
 
 
 }
+
+
+
+function countdown()
+{
+
+if (timers.length == 0) {
+    
+
+    return;
+}
+
+let output = document.getElementById("output1");
+output.textContent ="";
+for (let index = 0; index < timers.length; index++) {
+
+    timers[index]--;
+output.textContent += "Timer" + index + ":" + timers[index] + "/n";
+    if (timers[index] == 0) {
+        alert ("timer is out");
+timers.splice(index,1);
+    }
+
+
+    
+}
+
+
+
+}
+
+
+
+
 
