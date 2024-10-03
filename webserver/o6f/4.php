28732824413e479f4b3358aac5a5d5ebafd0f8ed
carl-beep-cloud <carl.hoel@elev.ga.ntig.se>
2024-10-03 15:11:58 +0200

    sista delen funkar inte

--- webserver/o6f/4.php
+++ webserver/o6f/4.php
@@ -26,6 +26,64 @@
 
 $g=0;
 
+if(isset($_POST['la'])) {
+
+  if (ctype_upper(substr($F, 0, 1))) {
+    
+  $g++;
+  
+   }
+  
+   else {
+      echo "First name needs to have first letter Uppercase:  ";
+   }
+  
+   if (ctype_upper(substr($E, 0, 1))) {
+  $g++;
+      
+   }
+  
+  
+   else {
+      echo "Last name needs to have first letter Uppercase: "; 
+   }
+  
+
+  
+
+
+
+$position = strpos($Ep, '@');
+
+
+$result = substr($Ep, $position, strlen($Ep));
+
+  
+   $result2 = substr($result,strlen($result)-2,strlen($result));
+
+   
+
+
+   if (str_contains($Ep,'@') && strlen($Ep) > 6 && strlen($result) > 4 &&  /* // todo  här är problemet */  preg_match('/./', $result2) <= 0) {
+      
+      $g++;
+
+
+   }
+  
+   else
+   {
+  
+      echo "E-Post needs to have a @ and be more then 6 letters!!! And 4 needs to be after the @ och sista 2 får inte innehålla .  ";
+   }
+  
+  if ($g == 3) {
+      echo $F . " " . $E . " med E-Posten " . $Ep . "  Allting ser ballt ut!!!" ;
+  }
+  
+  }
+   
+  
 
 
 ?>
