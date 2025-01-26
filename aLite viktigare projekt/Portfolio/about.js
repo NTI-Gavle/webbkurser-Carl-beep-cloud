13d20b862fe4a72c3af03cbef65ae1b5b1cd37cd
Fransic-Bacon <cfwhoel@gmail.com>
2025-01-26 20:08:41 +0100

    måste fixa buggar med  alert och med headern

--- aLite viktigare projekt/Portfolio/about.js
+++ aLite viktigare projekt/Portfolio/about.js
@@ -6,3 +6,18 @@ let om = document.getElementById("om");
 start.classList = "headerbox";
 projekt.classList="headerbox";
 om.classList="selected-headerbox";
+
+
+
+let diven = document.getElementById("alerten");
+
+
+function show(){
+    diven.classList.remove("hidden");
+    diven.classList.add("alert", "alert-dark");
+}
+
+function dontshow(){
+    diven.classList.add("hidden");
+    diven.classList.remove("alert", "alert-dark");
+}
--- aLite viktigare projekt/Portfolio/about.php
+++ aLite viktigare projekt/Portfolio/about.php
@@ -3,13 +3,28 @@ require "databas-connect.php";
 
 if (isset($_POST['nam']) && isset($_POST['age']) && isset($_POST['comment'])) {
     
-  
+    if(strlen($_POST['nam'])<2 ||  strlen($_POST['age'])<1|| strlen($_POST['comment'])<5)
+    {
+       ?>
+        <script>//show();
+            alert("Det funkar inte");</script>
+       <?php
+             //? header("Location: about.php#write-comment");
+    }
+
+    else{
+
+        ?>
+        <script>dontshow();</script>
+        <?php
+
         $stmt = $dbconn->prepare("INSERT INTO komen (namn, age, texten) VALUES (?, ?, ?)");
         $stmt->execute([$_POST['nam'], $_POST['age'], $_POST['comment']]);
         session_unset();
+        //? måste fixa
         header("Location: about.php#comment-section");
         exit;
- 
+    }
 
 }
 
@@ -44,6 +59,7 @@ if (isset($_POST['nam']) && isset($_POST['age']) && isset($_POST['comment'])) {
 
 <body>
     <?php include "header&fotter/header.html" ?>
+   
     <div class="about-container">
 
         <div class="about-container-part-1">
@@ -59,19 +75,19 @@ if (isset($_POST['nam']) && isset($_POST['age']) && isset($_POST['comment'])) {
                 </div>
 
                 <div class="social-media-atag-container">
-                    <a href="" class="social-media"> <img class="social-media-image" target="blank"
+                    <a href="https://www.instagram.com/fransicbaconfan/" target="blank" class="social-media"> <img class="social-media-image" target="blank"
                             src="Social-media-logo-folder\insta-logo.png" alt=""> </a>
 
-                    <a href="" class="social-media"> <img class="social-media-image" target="blank"
+                    <a href="https://im.qq.com/index/" class="social-media" target="blank"> <img class="social-media-image" target="blank"
                             src="Social-media-logo-folder\qq-logo.png" alt=""> </a>
 
-                    <a href="" class="social-media"> <img class="social-media-image" target="blank"
+                    <a href="https://telegram.org/" class="social-media" target="blank"> <img class="social-media-image" target="blank"
                             src="Social-media-logo-folder\telegram-logo.png" alt=""> </a>
 
-                    <a href="" class="social-media"> <img class="social-media-image" target="blank"
+                    <a href="https://www.wechat.com/" class="social-media" target="blank"> <img class="social-media-image" target="blank"
                             src="Social-media-logo-folder\we-chat-logo.png" alt=""> </a>
 
-                    <a href="" class="social-media"> <img class="social-media-image" target="blank"
+                    <a href="https://x.com/BenjaminDousa?mx=2" class="social-media" target="blank"> <img class="social-media-image" target="blank"
                             src="Social-media-logo-folder\x-logo.png" alt=""> </a>
                 </div>
                 <h2>E-Post: Tjingtjong@gmail.com</h2>
@@ -120,7 +136,7 @@ if (isset($_POST['nam']) && isset($_POST['age']) && isset($_POST['comment'])) {
         <h3> Skriv egen komentar </h3>
     </div>
 
-    <div class="form-container">
+    <div class="form-container" id="write-comment">
 
         <form action="" method="post">
             <label for="name">Namn:</label>
@@ -134,6 +150,9 @@ if (isset($_POST['nam']) && isset($_POST['age']) && isset($_POST['comment'])) {
 
             <button type="submit">Skicka</button>
         </form>
+        <div id="alerten" class="alert alert-dark hidden"  role="alert">
+  A simple dark alert—check it out!
+</div>
     </div>
 
     <?php include "header&fotter/fotter.html" ?>
